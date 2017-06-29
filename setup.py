6ea0fc3648b54445bf5ba49fce8784fa13369ffa
Grant-P-W <trduunze@gmail.com>
2017-06-29 16:21:40 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,77 +1,38 @@
-#!/usr/bin/env python
-import os
-import shutil
-import sys
+# Workaround for issue in Python 2.7.3
+# See http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+try:
+    # Try using ez_setup to install setuptools if not already installed.
+    from ez_setup import use_setuptools
+    use_setuptools()
+except ImportError:
+    # Ignore import error and assume Python 3 which already has setuptools.
+    pass
+
 from setuptools import setup, find_packages
 
-VERSION = '1.3.0a1'
 
-long_description = '''great lcd library'''
+classifiers = ['Development Status :: 4 - Beta',
+               'Operating System :: POSIX :: Linux',
+               'License :: OSI Approved :: MIT License',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Topic :: Software Development',
+               'Topic :: System :: Hardware']
 
-# The source dist comes with batteries included, the wheel can use pip to get the rest
-is_wheel = 'bdist_wheel' in sys.argv
-
-excluded = []
-if is_wheel:
-    excluded.append('extlibs.future')
-
-def exclude_package(pkg):
-    for exclude in excluded:
-        if pkg.startswith(exclude):
-            return True
-    return False
-
-def create_package_list(base_package):
-    return ([base_package] +
-            [base_package + '.' + pkg
-             for pkg
-             in find_packages(base_package)
-             if not exclude_package(pkg)])
-
-
-setup_info = dict(
-    # Metadata
-    name='lcdlib',
-    version=VERSION,
-    author='Grant Wodny',
-    author_email='trduunze@gmail.com',
-    url='https://github.com/Grant-P-W/lcd_lib/',
-    download_url='https://github.com/Grant-P-W/lcd_lib/',
-    description='raspberry pi lcd library',
-    long_description=long_description,
-    license='BSD',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: MacOS X',
-        'Environment :: Win32 (MS Windows)',
-        'Environment :: X11 Applications',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Topic :: Games/Entertainment',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-
-    # Package info
-    packages=create_package_list('lcdlib'),
-
-    # Add _ prefix to the names of temporary build dirs
-    options={
-        'build': {'build_base': '_build'},
-        #        'sdist': {'dist_dir': '_dist'},
-    },
-    zip_safe=True,
-)
-
-if is_wheel:
-    setup_info['install_requires'] = ['future']
-
-setup(**setup_info)
+setup(name              = 'lcdlib',
+      version           = '1.5.1',
+      author            = 'Grant Wodny',
+      author_email      = 'trduunze@gmail.com',
+      description       = 'Library to control a 16x2 lcd screen',
+      license           = 'MIT',
+      classifiers       = classifiers,
+      url               = 'https://github.com/Grant-P-W/',
+      dependency_links  = ['https://github.com/Grant-P-W/lcd_lib/'],
+      install_requires  = ['me'],
+      packages          = find_packages())
